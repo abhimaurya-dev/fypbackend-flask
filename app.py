7b492cf1a86046f0f0bba4651f2559cbb1d50850
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2023-05-25 16:44:34 +0530

    first commit

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def recommendCrop():
         query_array = list(map(float,query_array))
         query_array = np.array(query_array).reshape(1,-1)
         response_data={}
-        with open("mlmodel\\trainedModel\\trained_model",'rb') as f:
-          setMessage="NO MODEL"
-          model = pickle.load(f)
-          predict_result = model.predict(query_array)
-          response_data['success'] = 'true'
-          response_data['predicted_crop'] = predict_result[0]
+        # with open("mlmodel\\trainedModel\\trained_model",'rb') as f:
+        model = pickle.load(open("mlmodel\\trainedModel\\trained_model",'rb'));
+        setMessage="NO MODEL"
+        predict_result = model.predict(query_array)
+        response_data['success'] = 'true'
+        response_data['predicted_crop'] = predict_result[0]
         return response_data
     except:
         return {
